459746b67499be53c109d75c6985431b994b2714
ZhengliangQi <2025081004bkp@cic.care>
2026-04-18 20:30:58 -0400

    README file documentation

---
+++ auto_test_file.py
@@ -0,0 +1,17 @@
+#Automated testing
+
+import unittest
+
+def your_function_here(param):
+    pass #placeholder
+
+class TestYourFunction(unittest.TestCase):
+
+    def test_show_list():
+        pass
+    # ADD TESTS HERE
+    # NOTE: use self.assertTrue, self.assertFalse or self.assertEqual
+    pass #placeholder
+
+if __name__ == '__main__':
+    unittest.main()
